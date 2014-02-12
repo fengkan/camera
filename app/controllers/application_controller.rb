3d238b81f59a06f9c006363181c74405850dd23b
lixiao <xiao.li@moregg.com>
2014-02-12 17:48:29 +0800

    刚刚的跳转有问题

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    request.env['omniauth.origin'] || request.referrer || '/'
   end
 end
