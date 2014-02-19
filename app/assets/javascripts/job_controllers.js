a45efd9f9140e80252d9df859a6d9d85b9a2a055
lixiao <xiao.li@moregg.com>
2014-02-19 18:25:33 +0800

    检索出的第一个结果会被自动选中

--- app/assets/javascripts/job_controllers.js
+++ app/assets/javascripts/job_controllers.js
@@ -4,6 +4,13 @@ job.controller('jobId',function($scope,$http){
   $scope.query=function(){
     $http.post('/job/'+$scope.name+'.json','').success(function(data){
       $scope.results=data;
+      $scope.currentId=data[0].id;
     });
+  };
+  $scope.ifStopLi=function(id){
+    if(id==$scope.currentId)
+      return ' stopli';
+    else
+      return '';
   };
 });
