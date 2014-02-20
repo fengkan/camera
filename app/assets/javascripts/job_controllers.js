41f39b7e013db0c86bdd99be091e7abf13a04203
lixiao <xiao.li@moregg.com>
2014-02-20 14:06:09 +0800

    加载模型

--- app/assets/javascripts/job_controllers.js
+++ app/assets/javascripts/job_controllers.js
@@ -7,6 +7,24 @@ job.controller('jobId',function($scope,$http){
       $scope.currentId=data[0].id;
     });
   };
+  $scope.changeModel=function(newId){
+    if(model)
+      scene.remove(model);
+    var loader = new THREE.OBJMTLLoader();
+    loader.load( '/obj/'+newId+'/'+newId+'.obj', '/obj/'+newId+'/'+newId+'.mtl', function ( object ) {
+      object.position.x = 0;
+      object.position.y = 0;
+      object.position.z = 0;
+      object.rotation.x = 0;
+      object.rotation.y = 0;
+      object.rotation.z = -1.6;
+      object.scale.x = 1;
+      object.scale.y = 1;
+      object.scale.z = 1;
+      model=object;
+      scene.add( model );
+    } );
+  }
   $scope.ifStopLi=function(id){
     if(id==$scope.currentId)
       return ' stopli';
