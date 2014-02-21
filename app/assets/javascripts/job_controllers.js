5cb1285881f072c4a8461b72144a95d89d947258
lixiao <xiao.li@moregg.com>
2014-02-21 17:10:38 +0800

    处理了启动时的情况

--- app/assets/javascripts/job_controllers.js
+++ app/assets/javascripts/job_controllers.js
@@ -1,40 +1,39 @@
-var job=angular.module('job',[]);
-
-job.controller('jobId',function($scope,$http){
+angular.module('job',[]).controller('jobId',function($scope,$http){
   $scope.queryStatus={text:''};
   $scope.recommendations=[{name:'ccc'},{name:'777'}];
+  $scope.queriedName=$scope.recommendations[Math.floor($scope.recommendations.length*Math.random())].name;
   $scope.$watch('queriedName',function(){
     $http.post('/job/'+$scope.queriedName+'.json','').success(function(data){
-      $scope.queryStatus.text=$scope.name+' 的3D照片：';
-      $scope.results=data;
-      $scope.currentId=$scope.queriedName;
+      $scope.currentObj=data[0];
+      $scope.queryStatus.text=$scope.currentObj.name+' 的3D照片：';
     }).error(function(data){
       if($scope.name)
         $scope.queryStatus.text='找不到'+$scope.name+' 的3D照片！';
       else
         $scope.queryStatus.text='';
-      $scope.results=undefined;
-      $scope.randomRecommendation();
     });
   });
-  $scope.$watch('currentId',function(){
+  $scope.$watch('currentObj',function(){
     if(model)
       scene.remove(model);
-    var newId=$scope.currentId;
+    if(!$scope.currentObj)
+      return;
+    var newId=$scope.currentObj.name;
     var loader = new THREE.OBJMTLLoader();
     loader.load( '/obj/'+newId+'/'+newId+'.obj', '/obj/'+newId+'/'+newId+'.mtl', function ( object ) {
       model=object;
       scene.add( model );
-    } );
+    },true );
   });
   $scope.ifStopLi=function(name){
-    if(name==$scope.currentId)
+    if(!$scope.currentObj)
+      return '';
+    if(name==$scope.currentObj.name)
       return ' stopli';
     else
       return '';
   };
   $scope.randomRecommendation=function(){
-    $scope.currentId=$scope.recommendations[$scope.recommendations.length*Math.random()];
-    console.log($scope.currentId);
+    $scope.queriedName=$scope.recommendations[$scope.recommendations.length*Math.random()].name;
   };
 });
