8efa1423783ddc4f71524f0031c0da6bcafde3f0
lixiao <xiao.li@moregg.com>
2014-02-21 16:00:13 +0800

    基本实现了查看功能

--- app/assets/javascripts/job_controllers.js
+++ app/assets/javascripts/job_controllers.js
@@ -1,10 +1,20 @@
 var job=angular.module('job',[]);
 
 job.controller('jobId',function($scope,$http){
+  $scope.queryStatus={text:''};
+  $scope.recommendations=[{name:'ccc'},{name:'777'}];
   $scope.$watch('queriedName',function(){
     $http.post('/job/'+$scope.queriedName+'.json','').success(function(data){
+      $scope.queryStatus.text=$scope.name+' 的3D照片：';
       $scope.results=data;
-      $scope.currentId=data[0].id;
+      $scope.currentId=$scope.queriedName;
+    }).error(function(data){
+      if($scope.name)
+        $scope.queryStatus.text='找不到'+$scope.name+' 的3D照片！';
+      else
+        $scope.queryStatus.text='';
+      $scope.results=undefined;
+      $scope.randomRecommendation();
     });
   });
   $scope.$watch('currentId',function(){
@@ -13,23 +23,18 @@ job.controller('jobId',function($scope,$http){
     var newId=$scope.currentId;
     var loader = new THREE.OBJMTLLoader();
     loader.load( '/obj/'+newId+'/'+newId+'.obj', '/obj/'+newId+'/'+newId+'.mtl', function ( object ) {
-      object.position.x = 0;
-      object.position.y = 0;
-      object.position.z = 0;
-      object.rotation.x = 0;
-      object.rotation.y = 0;
-      object.rotation.z = -1.6;
-      object.scale.x = 1;
-      object.scale.y = 1;
-      object.scale.z = 1;
       model=object;
       scene.add( model );
     } );
   });
-  $scope.ifStopLi=function(id){
-    if(id==$scope.currentId)
+  $scope.ifStopLi=function(name){
+    if(name==$scope.currentId)
       return ' stopli';
     else
       return '';
+  };
+  $scope.randomRecommendation=function(){
+    $scope.currentId=$scope.recommendations[$scope.recommendations.length*Math.random()];
+    console.log($scope.currentId);
   };
 });
