56282424a4dc23bc664e78069a9995eb3d965e1e
Kan FENG <fengkan@gmail.com>
2014-02-27 12:57:42 +0800

    FEATURE - font and misc

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -2,7 +2,7 @@ class Shipment < ActiveRecord::Base
 	belongs_to :user
 	
 	def mixed_str
-		return (name.blank? ? "" : name) + " " + (address.blank? ? "" : address) + " " + (phone.blank? ? "" : phone)
+		return (name.blank? ? "" : name) + " " + (phone.blank? ? "" : phone) + " " + (address.blank? ? "" : address)
 		
 	end
 end
